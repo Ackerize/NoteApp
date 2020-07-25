3a9060aeb700ef30f5b3dea63ba571dab51e99f2
DavidVallecios <00078217@uca.edu.sv>
2020-07-24 21:29:27 -0600

    Vista registro terminada

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.noteapp
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //for changing status bar icon colors
+        /*for changing status bar icon colors
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        }
-        setContentView(R.layout.activity_login)
+        }*/
+
+        setContentView(R.layout.activity_register)
+        changeStatusBarColor()
+    }
+
+    private fun changeStatusBarColor() {
+        val window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        //window.setStatusBarColor(Color.TRANSPARENT);
+        window.statusBarColor = resources.getColor(R.color.register_bk_color)
     }
 }
